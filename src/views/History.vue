fc0463e3bcda28a783dec182aa362247d1721858
Jarret Nachtigal <jarretn1224@gmail.com>
2021-12-13 13:50:43 -0500

    initial commit + created History.vue + route and router link on app.vue

---
+++ src/views/History.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="history">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<script>
+// import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "history",
+    };
+  },
+  created: function () {},
+  methods: {},
+  setup() {},
+};
+</script>
