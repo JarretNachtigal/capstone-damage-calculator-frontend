5a4927b57bae63a7c32892f50ebc78d4b94fb0b2
Jarret Nachtigal <jarretn1224@gmail.com>
2021-12-15 16:34:14 -0500

    this shit broken lmao

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,28 +2,103 @@
   <div class="home">
     <body>
       <h1>{{ message }}</h1>
+      <!-- <h1>{{ abilities }}</h1> -->
       <!-- champions dropdown-->
       <div>
-        <form v-on:submit.prevent="createCalculation(currentCalculation)">
+        <form>
+          <!-- row 1 -->
           <div class="row">
-            <div class="col-md-5 offset-md-1">
-              <label for="champions">Choose an attacking champion:</label>
-              <input type="text" v-model="currentCalculation.champion_id_one" list="champions" />
-            </div>
-            <div class="col-md-5 offset-md-1">
-              <label for="champions">Choose a defending champion:</label>
-              <input type="text" v-model="currentCalculation.champion_id_two" list="champions" />
-            </div>
-            <!-- datalist for champion input -->
+            <!-- datalist for champions -->
             <datalist id="champions">
               <option v-for="champ in champions" :key="champ.id">
                 {{ getChampName(champ.id) }}
               </option>
             </datalist>
+            <!-- datalist for abilities -->
+            <datalist id="abilities">
+              <option v-for="ability in abilities" :key="ability.id">
+                {{ getAbilityName(ability.id) }}
+              </option>
+            </datalist>
+            <!-- datalist for champion levels -->
+            <datalist id="levels">
+              <option v-for="index in 18" :key="index">
+                {{ index }}
+              </option>
+            </datalist>
+            <!-- datalist for ability levels -->
+            <datalist id="ability levels">
+              <option v-for="index in 5" :key="index">
+                {{ index }}
+              </option>
+            </datalist>
+            <div class="col-md-3">
+              <label for="champions">Choose an attacking champion:</label>
+            </div>
+            <div class="col-md-3">
+              <input type="text" v-model="championOneName" list="champions" />
+            </div>
+            <div class="col-md-3">
+              <label for="champions">Choose a defending champion:</label>
+            </div>
+            <div class="col-md-3">
+              <input type="text" v-model="championTwoName" list="champions" />
+            </div>
           </div>
+          <!-- row 2 -->
           <div class="row">
-            <div class="col-sm-4 offset-5">
-              <input type="submit" />
+            <div class="col-md-3">
+              <label for="levelsone">Attacking champion level:</label>
+            </div>
+            <div class="col-md-3">
+              <input type="text" v-model="currentCalculation.champ_one_level" list="levels" />
+            </div>
+            <div class="col-md-3">
+              <label for="levelstwo">Defending champion Level:</label>
+            </div>
+            <div class="col-md-3">
+              <input type="text" v-model="currentCalculation.champ_two_level" list="levels" />
+            </div>
+          </div>
+          <!-- row 3 -->
+          <div class="row">
+            <div class="col-md-3">
+              <label for="abilities">Choose an ability:</label>
+            </div>
+            <div class="col-md-3">
+              <input type="text" v-model="abilityName" list="abilities" />
+            </div>
+            <div class="col-md-3">
+              <label for="defendinghp">Defending champion hp: (default full)</label>
+            </div>
+            <div class="col-md-3">
+              <input type="text" v-model="currentCalculation.defending_champion_current_hp" />
+            </div>
+          </div>
+          <!-- row 4 -->
+          <div class="row">
+            <div class="col-md-3">
+              <label for="ability levels">Ability level:</label>
+            </div>
+            <div class="col-md-3">
+              <input type="text" v-model="currentCalculation.ability_level" list="ability levels" />
+            </div>
+            <!-- <div class="col-md-3">
+              <label for="item2">Choose an item:</label>
+            </div>
+            <div class="col-md-3">
+              <input type="text" v-model="defendingItemOne" list="items" />
+            </div> -->
+          </div>
+          <!-- submit button row -->
+          <div class="row">
+            <div class="col-sm-6">
+              <button
+                type="button"
+                v-on:click="createCalculation(currentCalculation, championOneName, championTwoName, abilityName)"
+              >
+                submit
+              </button>
             </div>
           </div>
         </form>
@@ -47,11 +122,17 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Home",
+      message: "New Calculation",
       champions: [],
       abilities: [],
       currentCalculation: {},
       championNames: [],
+      // used to hold info before translating names into ids and requesting new calculation from backend
+      championOneName: "",
+      championTwoName: "",
+      abilityName: "",
+      attackingItemOne: "",
+      defendingItemOne: "",
     };
   },
   created: function () {
@@ -63,11 +144,30 @@ export default {
       this.abilities = response.data;
       console.log(this.abilities);
     });
-    // this.championNames = this.champions.map(); // make a hash with :name and :id
   },
   methods: {
-    createCalculation() {
-      axios.post("http://localhost:3000/calculations", this.currentCalculation).then((response) => {
+    // fields of currentCalculation need to be translated from newThing to id first
+    createCalculation(currentCalculation, championOneName, championTwoName, abilityName) {
+      currentCalculation.champion_id_one = this.champions.findIndex(function (champ) {
+        if (champ.name === championOneName) {
+          return champ.id;
+        }
+      });
+      // this.currentCalculation.champion_id_one++;
+      currentCalculation.champion_id_two = this.champions.findIndex(function (champ) {
+        if (champ.name === championTwoName) {
+          return champ.id;
+        }
+      });
+      // this.currentCalculation.champion_id_two++;
+      currentCalculation.ability_id = this.abilities.findIndex(function (ability) {
+        if (ability.name === abilityName) {
+          return ability.id;
+        }
+      });
+      // this.currentCalculation.ability_id++;
+      console.log("this is the thing", this.currentCalculation);
+      axios.post("http://localhost:3000/calculations", currentCalculation).then((response) => {
         console.log(response.data);
       });
     },
@@ -77,6 +177,18 @@ export default {
       });
       let championName = this.champions[championId].name;
       return championName;
+    },
+    getAbilityName: function (calc_id) {
+      let abilityId = this.abilities.findIndex(function (champ) {
+        return champ.id == calc_id;
+      });
+      let abilityName = this.abilities[abilityId].name;
+      return abilityName;
+    },
+    // this will get called when an attacking champion is selected
+    getAbilities(champ_id) {
+      this.abilities = this.champions;
+      console.log(champ_id);
     },
   },
 };
